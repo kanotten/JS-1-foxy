5e53cb06a82045741e11329df0a3b76a374f9d05
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-02 13:37:36 +0100

    added basic script code

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log ("hello");
+
+//variables
+var myName = "Line";
+console.log(myName); // output : Line
+
+let age = 31;
+console.log(age); // output : 31
+
+const BRAND = "BMW";
+console.log(BRAND); // output : BMW
+
+let greeting = "Hello, World";
+console.log(typeof greeting); // typeof shows what is in this variable, greeting is a string
+
+let isHappy = true;
+console.log(isHappy); // boleans in this case is true.
