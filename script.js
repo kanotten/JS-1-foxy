0cda9b2da742761478a6c67f600f4ad6799d213d
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-02 14:15:11 +0100

    added more basic script: arrays and consolelogs

--- script.js
+++ script.js
@@ -14,3 +14,39 @@ console.log(typeof greeting); // typeof shows what is in this variable, greeting
 
 let isHappy = true;
 console.log(isHappy); // boleans in this case is true.
+
+//Object
+let person = {
+    firstname: "Hesham",
+    lastname: "El Masry",
+    age: 31
+};
+console.log(person); // will show persons content
+console.log(person.firstname); // shows only the "person" first name
+console.log(person.lastname); // -- last name
+console.log(person.age); // -- age for object/person
+
+
+let car = {
+    brand: "Volvo",
+    model: 1992,
+    condition: "Used",
+    price: 10000,
+    service: "yearly service",
+    eucertified: 2026
+};
+console.log(car); // car is the object, when consolelog it shows all the info/contents.
+console.log(" I have a "
++ car.brand + " and it is "
++ car.model + " and it is "
++ car.condition + " and the price is "
++ car.price + " NOK"); // this is how to add numbers and txt together.
+
+//Array
+
+let colors = ["Red", "Green", "Blue"];
+
+let random = [55656, "Green", { name: "Hesh", age: 31},
+["boy", "girl", "whatever"]
+]; // Array can holw many thing, as is a larger storage box. Positions start with 0,1,2,3 etc so first is 0.
+console.log(random[0]);
